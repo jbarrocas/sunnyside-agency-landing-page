79f463d9aff62334d9b451153c70d230c7b02896
José Barrocas <128058496+jbarrocas@users.noreply.github.com>
2023-04-13 10:37:08 +0100

    Icons_resolved

--- js/main.js
+++ js/main.js
@@ -13,22 +13,22 @@ learnMore.forEach(element => {
 })
 
 
-let contacts = document.querySelectorAll('.contacts');
-contacts.forEach(element => {
-    element.addEventListener('mouseenter', eventArgs =>{
-        let contactOvered = eventArgs.target;
-        let whiteIcon = contactOvered.nextElementSibling;
-        contactOvered.classList.add('hide');
-        whiteIcon.classList.remove('hide');        
-    })
-})
+// let contacts = document.querySelectorAll('.contacts');
+// contacts.forEach(element => {
+//     element.addEventListener('mouseenter', eventArgs =>{
+//         let contactOvered = eventArgs.target;
+//         let whiteIcon = contactOvered.nextElementSibling;
+//         contactOvered.classList.add('hide');
+//         whiteIcon.classList.remove('hide');        
+//     })
+// })
 
-let overedContact = document.querySelectorAll('.white-contact');
-overedContact.forEach(element =>{
-    element.addEventListener('mouseleave', eventArgs =>{
-        let leavedContact = eventArgs.target;
-        let darkIcon = leavedContact.previousElementSibling;
-        leavedContact.classList.add('hide');
-        darkIcon.classList.remove('hide');
-    })
-})
+// let overedContact = document.querySelectorAll('.white-contact');
+// overedContact.forEach(element =>{
+//     element.addEventListener('mouseleave', eventArgs =>{
+//         let leavedContact = eventArgs.target;
+//         let darkIcon = leavedContact.previousElementSibling;
+//         leavedContact.classList.add('hide');
+//         darkIcon.classList.remove('hide');
+//     })
+// })
